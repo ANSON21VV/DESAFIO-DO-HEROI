2753f0c84bd09eede70630ca883c87c32e5d5f3c
ANSON21VV <anderosncirino@gmail.com>
2024-06-26 00:54:31 -0300

    vaioo

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("oi")
+let idade=19
+let nome="andeROS"
+//quando voçê ultilizar a expressão logica NOT"!"" todo vezes que voçê usar ! o valor ficar mundado pela quantida de vezes "!"
+let RESUL= !((nome==="andeROS") && (idade<=20))
+if(RESUL===true){
+    console.log("oi")
+    
+
+    }
+
+ else if(RESUL!==true ) {
+    console.log("oiiii")
+}
+
+
+
+  a
